73577685ee6898580e8f490db7c477893b7e48c8
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2024-03-08 01:31:47 -0300

    feat: created books crud with express using array stored data

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const routes = {
-    "/": "Node API",
-    "/books": "Books Route",
-    "/authors": "Authors Route"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(routes[req.url]);
-});
-
-server.listen(PORT, () => {
-    console.log("Server is listening port 3000...");
+app.listen(PORT, () => {
+	console.log("Server is listening port 3000...");
 });
